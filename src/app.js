fdfff2d9b8c892c05af6dd62803e6e0b713bb7be
VS1379 <valentin.silva.user@gmail.com>
2023-11-07 18:37:19 -0300

    adjustmen of footer on desktop

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const port = 8000;
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -9,6 +9,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+app.get("/login", (req, res) =>{
+  res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
 
 app.listen(port, () => {
   console.log(`server on in http://localhost:${port}`);
