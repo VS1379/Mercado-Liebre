05d4c686ccea41a11d0bd9090e1cadf8452b5e79
VS1379 <valentin.silva.user@gmail.com>
2023-11-08 00:51:24 -0300

    form update

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/register.html"))
 });
