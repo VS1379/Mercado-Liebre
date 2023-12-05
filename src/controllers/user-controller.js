9c7fa63f41364ced29e1e68f62ff04cc51ba0308
VS1379 <valentin.silva.user@gmail.com>
2023-12-05 13:13:31 -0300

    register

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,16 +1,36 @@
 const controller = {
   register: (req, res) => {
     res.render("register");
-    res.send(req.body)
-    console.log(req.body);
   },
   newUser: (req, res) => {
-    res.send(req.body)
+    res.send(req.body);
+    console.log("--v--");
     console.log(req.body);
+    console.log("--^--");
+    res.redirect("/");
   },
   login: (req, res) => {
     res.render("login");
   },
 };
+function fileRead(localization) {
+  //leer un archivo
+  const fs = require("fs");
+  let file = fs.readFileSync(localization, "utf-8");
+  file = JSON.parse(file);
+  return file;
+}
 
+function fileWrite(localization, content) {
+  //escribir un archivo "../db/users.json"
+  const fs = require("fs");
+  fs.writeFileSync(localization, content);
+}
+
+function fileUpdate(localization, content) {
+  //actualiza un archivo
+  const fs = require("fs");
+  content = JSON.stringify(content);
+  fs.appendFileSync(localization, content);
+}
 module.exports = controller;
--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -3,6 +3,7 @@ const controller = require("../controllers/product-controller");
 
 const router = express.Router();
 
+router.get("/", controller.product);
 router.get("/product", controller.product);
 
 module.exports = router;
